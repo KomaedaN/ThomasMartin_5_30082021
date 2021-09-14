268857ffdf75b950bf67abf77d775f6bc0a436de
Thomas <thomasmrt200@gmail.com>
2021-09-14 14:18:23 +0200

    ours index

---
+++ js/index.js
@@ -0,0 +1,30 @@
+function afficherOurs(produit) {
+    const oursCard = document.getElementById("oursCard");
+
+    const template = document.importNode(oursCard.content, true);
+
+    template.getElementById("imageProduitOurs").src = produit.imageUrl;
+    template.getElementById("nameProduitOurs").textContent = produit.name;
+    template.getElementById("descriptionProduitOurs").textContent = produit.description;
+    template.getElementById("priceProduitOurs").textContent = `${produit.price / 100}€`;
+    template.getElementById("produitPage").href = `./produit.html?id=${produit._id}`
+
+    document.getElementById("listProduit").appendChild(template)
+}
+
+async function getListOurs () {
+    return fetch("http://localhost:3000/api/teddies")
+        .then((httpBodyResponse) => httpBodyResponse.json())
+        .then((produits) => {
+            produits.forEach((produit) => {
+                afficherOurs(produit)
+            })
+        })
+        .catch((error) => {
+            alert("erreur")
+        })
+}
+
+;(async () => {
+    getListOurs()
+})()
